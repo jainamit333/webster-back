3f141c15b94cf6c7b90ff49e1deb1168635e6bed
Amit Jain <Amit.Jain@makemytrip.com>
2016-07-14 10:32:00 +0530

    blanck project

---
+++ src/main/java/com/webster/WebsterBackApplication.java
@@ -0,0 +1,12 @@
+package com.webster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsterBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsterBackApplication.class, args);
+	}
+}
